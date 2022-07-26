0f42c68196454faf1bc19743b2961fc541fa9622
yzx <819008851@qq.com>
2022-07-26 18:46:08 +0800

    redis配置

--- src/main/java/com/yzx/reggie/ReggieTakeOutApplication.java
+++ src/main/java/com/yzx/reggie/ReggieTakeOutApplication.java
@@ -1,5 +1,7 @@
 package com.yzx.reggie;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @Slf4j
 @EnableTransactionManagement
+//@EnableCreateCacheAnnotation
+//@EnableMethodCache(basePackages = "com.yzx.reggie")
 public class ReggieTakeOutApplication {
 
     public static void main(String[] args) {
